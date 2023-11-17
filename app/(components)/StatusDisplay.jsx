4313da4fd9592110cf37d881e141d93ddf03c221
Nate Naranjo <natenaranjodev@gmail.com>
2023-11-17 09:56:14 -0600

    Ticket-App - completed the course and submitting final project

---
+++ app/(components)/StatusDisplay.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+
+const StatusDisplay = ({ status }) => {
+  const getColor = (status) => {
+    let color = "bg-slate-700";
+    switch (status.toLowerCase()) {
+      case "done":
+        color = "bg-green-200";
+        return color;
+        break;
+
+      case "in progress":
+        color = "bg-yellow-200";
+        return color;
+        break;
+
+      case "testing":
+        color = "bg-blue-200";
+        return color;
+        break;
+
+      case "not started":
+        color = "bg-red-200";
+        return color;
+        break;
+    }
+    return color;
+  };
+
+  return (
+    <span
+      className={`inline-block rounded-full px-2 py-1 text-xs font-semibold text-gray-700 ${getColor(
+        status
+      )}`}
+    >
+      {status}
+    </span>
+  );
+};
+
+export default StatusDisplay;
---
+++ app/(components)/TicketForm.jsx
@@ -0,0 +1,187 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import React, { useState } from "react";
+
+const TicketForm = ({ ticket }) => {
+  const EDITMODE = ticket._id === "new" ? false : true;
+  const router = useRouter();
+
+  const startingTicketData = {
+    title: "",
+    description: "",
+    priority: 1,
+    progress: 0,
+    status: "not started",
+    category: "New Feature",
+  };
+
+  if (EDITMODE) {
+    startingTicketData["title"] = ticket.title;
+    startingTicketData["description"] = ticket.description;
+    startingTicketData["priority"] = ticket.priority;
+    startingTicketData["progress"] = ticket.progress;
+    startingTicketData["status"] = ticket.status;
+    startingTicketData["category"] = ticket.category;
+  }
+
+  const [formData, setFormData] = useState(startingTicketData);
+
+  const handleChange = (e) => {
+    const value = e.target.value;
+    const name = e.target.name;
+
+    setFormData((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (EDITMODE) {
+      const res = await fetch(`/api/Tickets/${ticket._id}`, {
+        method: "PUT",
+        body: JSON.stringify({ formData }),
+        "content-type": "application/json",
+      });
+
+      if (!res.ok) {
+        throw new Error("Failed to update ticket.");
+      }
+    } else {
+      const res = await fetch("/api/Tickets", {
+        method: "POST",
+        body: JSON.stringify({ formData }),
+        "content-type": "application/json",
+      });
+
+      if (!res.ok) {
+        throw new Error("Failed to create ticket.");
+      }
+    }
+
+    router.refresh();
+    router.push("/");
+  };
+
+  return (
+    <div className="flex justify-center">
+      <form
+        className="flex flex-col gap-3 w-1/2"
+        method="post"
+        onSubmit={handleSubmit}
+      >
+        <h3>{EDITMODE ? "Editing Your Ticket" : "Create Your Ticket"}</h3>
+        <label htmlFor="title">Title</label>
+        <input
+          id="title"
+          name="title"
+          type="text"
+          value={formData.title}
+          onChange={handleChange}
+          required={true}
+        />
+
+        <label htmlFor="description">Description</label>
+        <textarea
+          id="description"
+          name="description"
+          value={formData.description}
+          onChange={handleChange}
+          required={true}
+          rows={5}
+        />
+
+        <label htmlFor="category">Category</label>
+        <select
+          name="category"
+          value={formData.category}
+          onChange={handleChange}
+        >
+          <option value="New Feature">New Feature</option>
+          <option value="Bug">Bug</option>
+          <option value="Enchancement">Enchancement</option>
+          <option value="Other">Other</option>
+        </select>
+
+        <label htmlFor="priority">Priority</label>
+        <div>
+          <input
+            id="priority-1"
+            name="priority"
+            type="radio"
+            onChange={handleChange}
+            value={1}
+            checked={formData.priority == 1}
+          />
+          <label htmlFor="priority-1">1</label>
+          <input
+            id="priority-2"
+            name="priority"
+            type="radio"
+            onChange={handleChange}
+            value={2}
+            checked={formData.priority == 2}
+          />
+          <label htmlFor="priority-2">2</label>
+          <input
+            id="priority-3"
+            name="priority"
+            type="radio"
+            onChange={handleChange}
+            value={3}
+            checked={formData.priority == 3}
+          />
+          <label htmlFor="priority-3">3</label>
+          <input
+            id="priority-4"
+            name="priority"
+            type="radio"
+            onChange={handleChange}
+            value={4}
+            checked={formData.priority == 4}
+          />
+          <label htmlFor="priority-4">4</label>
+          <input
+            id="priority-5"
+            name="priority"
+            type="radio"
+            onChange={handleChange}
+            value={5}
+            checked={formData.priority == 5}
+          />
+          <label htmlFor="priority-5">5</label>
+        </div>
+
+        <label htmlFor="progress">Progress</label>
+        <input
+          type="range"
+          name="progress"
+          value={formData.progress}
+          min="0"
+          max="100"
+          step="1"
+          onChange={handleChange}
+        />
+
+        <label htmlFor="status">Status</label>
+        <select name="status" value={formData.status} onChange={handleChange}>
+          <option value="not started">Not Started</option>
+          <option value="in progress">In Progress</option>
+          <option value="testing">Testing</option>
+          <option value="completed">Completed</option>
+        </select>
+
+        <input
+          type="submit"
+          className="btn"
+          value={EDITMODE ? "Update Ticket" : "Create Ticket"}
+        />
+      </form>
+    </div>
+  );
+};
+
+export default TicketForm;
